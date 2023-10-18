f548bbad2ee50f1fe327f70d798a0bda2caffaf7
maxchamberlain <mchamberlain@shopvitality.com>
2023-10-17 18:13:40 -0600

    Initial Commit

---
+++ src/routes/api/education/+server.ts
@@ -0,0 +1,6 @@
+export async function GET() {
+	const courses = [];
+	//   return new Response(JSON.stringify(sortedData.filter((e: any) => e.visibility === 'public' && e.language)), {
+	//     status: 201
+	//   })
+}
---
+++ src/routes/education/+page.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+</script>
+
+<main class="w-full max-w-7xl p-4 mx-auto" />
